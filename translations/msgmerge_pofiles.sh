64171ba838f0187e765a4dba611205e34d54e46b
Eric Gallager <egall@gwmail.gwu.edu>
2022-09-23 03:15:05 -0400

    minor commentary update

--- translations/msgmerge_pofiles.sh
+++ translations/msgmerge_pofiles.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# First, run wmlxgettext from your wesnoth tools directory like this:
+# ./wmlxgettext --domain=wesnoth-The_Earths_Gut --directory=/Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.16/data/add-ons/The_Earths_Gut --recursive --warnall --no-sort-by-file -o /Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.16/data/add-ons/The_Earths_Gut/translations/TEG.pot
+# (with edits to correct paths as necessary)
+
 for lang in ja; do msgmerge --previous --update --lang=${lang} ${lang}/wesnoth-The_Earths_Gut.po TEG.pot; done
