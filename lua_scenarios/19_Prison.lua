5bc1807a87935dfb7224641e18f33eb42dd958b4
Eric Gallager <egall@gwmail.gwu.edu>
2023-02-10 21:53:03 -0500

    some changes requested by @Konrad22

--- lua_scenarios/19_Prison.lua
+++ lua_scenarios/19_Prison.lua
@@ -1,4 +1,5 @@
 if not wml_actions then wml_actions = {} end
+local debug_utils = pcall(wesnoth.dofile, "~add-ons/Wesnoth_Lua_Pack/debug_utils.lua")
 
 function wml_actions.prison_put_prisoners(udArgs)
 	local variable = udArgs.variable
