60fa75af93685a39997c2556f009b9c84b084a86
Eric Gallager <egall@gwmail.gwu.edu>
2023-08-29 03:32:23 -0400

    translations update

--- translations/msgmerge_pofiles.sh
+++ translations/msgmerge_pofiles.sh
@@ -4,7 +4,7 @@
 # ./wmlxgettext --domain=wesnoth-The_Earths_Gut --directory=/Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.16/data/add-ons/The_Earths_Gut --recursive --warnall --no-sort-by-file -o /Users/ericgallager/Library/Containers/org.wesnoth.Wesnoth/Data/Library/Application\ Support/Wesnoth_1.16/data/add-ons/The_Earths_Gut/translations/TEG.pot
 # (with edits to correct paths as necessary)
 
-for mylang in ja; do
+for mylang in cs ja; do
 	msgmerge --previous --update --lang=${mylang} wesnoth-The_Earths_Gut/${mylang}.po TEG.pot;
 	msgfmt -o wesnoth-The_Earths_Gut/${mylang}.mo wesnoth-The_Earths_Gut/${mylang}.po;
 done
